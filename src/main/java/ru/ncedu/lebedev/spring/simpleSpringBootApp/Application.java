5ed840723ae6fb4cca021212431624c24cb21046
Artem Lebedev <aurrum64@gmail.com>
2019-08-02 16:42:41 +0300

    Testing GIT pushing from work

--- src/main/java/ru/ncedu/lebedev/spring/simpleSpringBootApp/Application.java
+++ src/main/java/ru/ncedu/lebedev/spring/simpleSpringBootApp/Application.java
@@ -3,6 +3,8 @@ package ru.ncedu.lebedev.spring.simpleSpringBootApp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//comment to check git working from work
+
 @SpringBootApplication
 public class Application {
 
